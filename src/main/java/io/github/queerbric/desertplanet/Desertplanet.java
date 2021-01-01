ba115fe36c44559839b8871cbcbfcc4e1f164f2a
SuperCoder7979 <k.pranav@gmail.com>
2020-12-31 19:38:09 -0500

    Initial commit

---
+++ src/main/java/io/github/queerbric/desertplanet/Desertplanet.java
@@ -0,0 +1,10 @@
+package io.github.queerbric.desertplanet;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Desertplanet implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/io/github/queerbric/desertplanet/client/DesertplanetClient.java
@@ -0,0 +1,13 @@
+package io.github.queerbric.desertplanet.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class DesertplanetClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
